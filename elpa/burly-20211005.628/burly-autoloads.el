1258dfaac060fb6d4036acd06d290e4c3181d7b7
Adam Porter <adam@alphapapa.net>
2021-10-05 06:29:08 -0500

    Upgrade: Burly

--- elpa/burly-20211005.628/burly-autoloads.el
+++ elpa/burly-20211005.628/burly-autoloads.el
@@ -72,7 +72,7 @@ the mode if ARG is nil, omitted, or is a positive number.
 Disable the mode if ARG is a negative number.
 
 To check whether the minor mode is enabled in the current buffer,
-evaluate `(default-value 'burly-tabs-mode)'.
+evaluate `(default-value \\='burly-tabs-mode)'.
 
 The mode's hook is called both when the mode is enabled and when
 it is disabled.
--- elpa/burly-20211005.628/burly-pkg.el
+++ elpa/burly-20211005.628/burly-pkg.el
@@ -1,4 +1,4 @@
-(define-package "burly" "20210930.2126" "Save and restore frame/window configurations with buffers"
+(define-package "burly" "20211005.628" "Save and restore frame/window configurations with buffers"
   '((emacs "28.0")
     (map "2.1"))
   :authors
