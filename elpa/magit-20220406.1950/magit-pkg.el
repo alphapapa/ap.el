79382c633235ae6a799f4acb98263c4c3d7b38ed
Adam Porter <adam@alphapapa.net>
2022-04-07 08:54:06 -0500

    Upgrade: Magit, Forge, and related packages

---
+++ elpa/magit-20220406.1950/magit-pkg.el
@@ -0,0 +1,18 @@
+(define-package "magit" "20220406.1950" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (dash "20210826")
+    (git-commit "20220222")
+    (magit-section "20220325")
+    (transient "20220325")
+    (with-editor "20220318"))
+  :commit "0bbf31a2efd769ac2a4734358b4da08a596e9514" :authors
+  '(("Marius Vollmer" . "marius.vollmer@gmail.com")
+    ("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("git" "tools" "vc")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/transient-20220406.1156/transient-pkg.el
@@ -0,0 +1,12 @@
+(define-package "transient" "20220406.1156" "Transient commands"
+  '((emacs "25.1"))
+  :commit "81b2b912de679749a52a4f5faf5fd02b36465431" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("bindings")
+  :url "https://github.com/magit/transient")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
