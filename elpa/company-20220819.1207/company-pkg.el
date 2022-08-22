45ff506d90b12e850e8090a290d3be6b22d639bf
Adam Porter <adam@alphapapa.net>
2022-08-22 17:28:38 -0500

    Upgrade: company

---
+++ elpa/company-20220819.1207/company-pkg.el
@@ -0,0 +1,12 @@
+(define-package "company" "20220819.1207" "Modular text completion framework"
+  '((emacs "25.1"))
+  :commit "a96295c055a6938458588b9933e5c0a3fcfae1e5" :authors
+  '(("Nikolaj Schumacher"))
+  :maintainer
+  '("Dmitry Gutov" . "dgutov@yandex.ru")
+  :keywords
+  '("abbrev" "convenience" "matching")
+  :url "http://company-mode.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
