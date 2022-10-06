1376962a41b226dfe2abeaba9f76352f6ac8a010
Adam Porter <adam@alphapapa.net>
2022-10-05 20:17:45 -0500

    Add: (ap/org-tree-to-indirect-buffer)

--- init.el
+++ init.el
@@ -541,6 +541,35 @@ the file!"
       :background ,(face-attribute 'org-scheduled-previously :foreground))))
 
   :config
+
+  (defun ap/org-tree-to-indirect-buffer (&optional arg)
+    "Create indirect buffer and narrow it to current subtree.
+The buffer is named after the subtree heading, with the filename
+appended.  If a buffer by that name already exists, it is
+selected instead of creating a new buffer."
+    ;; TODO: Upstream this into Org as one of the options for `org-indirect-buffer-display'.
+    (interactive "P")
+    (let* ((new-buffer-p)
+           (pos (point))
+           (buffer-name (let* ((heading (org-get-heading t t))
+                               (level (org-outline-level))
+                               (face (intern (concat "outline-" (number-to-string level))))
+                               (heading-string (propertize (org-link-display-format heading)
+                                                           'face face)))
+                          (concat heading-string "::" (buffer-name))))
+           (new-buffer (or (get-buffer buffer-name)
+                           (prog1 (condition-case nil
+                                      (make-indirect-buffer (current-buffer) buffer-name 'clone)
+                                    (error (make-indirect-buffer (current-buffer) buffer-name)))
+                             (setq new-buffer-p t)))))
+      (switch-to-buffer new-buffer)
+      (when new-buffer-p
+        ;; I don't understand why setting the point again is necessary, but it is.
+        (goto-char pos)
+        (rename-buffer buffer-name)
+        (org-narrow-to-subtree))))
+  (advice-add #'org-tree-to-indirect-buffer :override #'ap/org-tree-to-indirect-buffer)
+
   (defun ap/org-agenda-goto-heading-in-indirect-buffer (&optional switch-to)
     "Go to the current agenda headline in an indirect buffer. If SWITCH-TO is non-nil, close the org-agenda window."
     (interactive)
