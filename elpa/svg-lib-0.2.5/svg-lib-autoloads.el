bb3d800f064e548d5fd5e2e83c99385d8dea4d31
Adam Porter <adam@alphapapa.net>
2022-10-10 22:29:07 -0500

    Install: svg-lib

---
+++ elpa/svg-lib-0.2.5/svg-lib-autoloads.el
@@ -0,0 +1,27 @@
+;;; svg-lib-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "svg-lib" "svg-lib.el" (0 0 0 0))
+;;; Generated autoloads from svg-lib.el
+
+(register-definition-prefixes "svg-lib" '("svg-lib-"))
+
+;;;***
+
+;;;### (autoloads nil nil ("svg-lib-demo.el" "svg-lib-pkg.el") (0
+;;;;;;  0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; svg-lib-autoloads.el ends here
---
+++ elpa/svg-lib-0.2.5/svg-lib-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from svg-lib.el  -*- no-byte-compile: t -*-
+(define-package "svg-lib" "0.2.5" "SVG tags, progress bars & icons" '((emacs "27.1")) :commit "0486c9453449771bc3f5872f70bc5cb23580d0f4" :maintainer '("Nicolas P. Rougier" . "Nicolas.Rougier@inria.fr") :keywords '("svg" "icons" "tags" "convenience") :url "https://github.com/rougier/svg-lib")
