d39a1d97cf0f36aa40fb3d7f55c7580e2d99405f
Adam Porter <adam@alphapapa.net>
2022-10-10 22:28:46 -0500

    Upgrade: plz

--- elpa/plz-0.1/plz-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from plz.el  -*- no-byte-compile: t -*-
-(define-package "plz" "0.1" "HTTP library" '((emacs "26.3")) :commit "9156de9c59c1836139db1b600f4056454b42dfd2" :authors '(("Adam Porter" . "adam@alphapapa.net")) :maintainer '("Adam Porter" . "adam@alphapapa.net") :keywords '("comm" "network" "http") :url "https://github.com/alphapapa/plz.el")
