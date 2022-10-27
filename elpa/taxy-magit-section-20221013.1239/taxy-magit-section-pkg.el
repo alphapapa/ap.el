20c08f33e70315d7c1c4fa9b0735acef017818e1
Adam Porter <adam@alphapapa.net>
2022-10-27 09:38:58 -0500

    Upgrade: taxy-magit-section

--- elpa/taxy-magit-section-20221013.1239/taxy-magit-section-pkg.el
+++ elpa/taxy-magit-section-20221013.1239/taxy-magit-section-pkg.el
@@ -1,6 +1,10 @@
-(define-package "taxy-magit-section" "20220329.1043" "View Taxy structs in a Magit Section buffer"
+(define-package
+  #("taxy-magit-section" 0 18
+    (face nil))
+  "20221013.1239" "View Taxy structs in a Magit Section buffer"
   '((emacs "26.3")
-    (magit-section "3.2.1"))
+    (magit-section "3.2.1")
+    (taxy "0.10"))
   :authors
   '(("Adam Porter" . "adam@alphapapa.net"))
   :maintainer
