1c3066f0559474c9686bebe211771367555a38e8
Adam Porter <adam@alphapapa.net>
2023-02-24 10:07:19 -0600

    Upgrade: xr

---
+++ elpa/xr-1.23/xr-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from xr.el  -*- no-byte-compile: t -*-
+(define-package "xr" "1.23" "Convert string regexp to rx notation" '((emacs "26.1")) :commit "74906f6d104b7daae162dc32684e0443e80fdb03" :authors '(("Mattias Engdegård" . "mattiase@acm.org")) :maintainer '("Mattias Engdegård" . "mattiase@acm.org") :keywords '("lisp" "regexps") :url "https://github.com/mattiase/xr")
