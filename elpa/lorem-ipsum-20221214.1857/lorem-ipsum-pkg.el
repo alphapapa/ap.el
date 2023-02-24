cddb960b68e85ec362fdb25735988fb38eae94d6
Adam Porter <adam@alphapapa.net>
2023-02-24 10:06:37 -0600

    Install: lorem-ipsum

---
+++ elpa/lorem-ipsum-20221214.1857/lorem-ipsum-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from lorem-ipsum.el  -*- no-byte-compile: t -*-
+(define-package #("lorem-ipsum" 0 11 (face nil)) "20221214.1857" "Insert dummy pseudo Latin text" 'nil :commit "4e87a899868e908a7a9e1812831d76c8d072f885" :authors '(("Jean-Philippe Theberge" . "jphil21@sourceforge.net")) :maintainer '("Joe Schafer" . "joe@jschaf.com") :keywords '("tools" "language" "convenience") :url "https://github.com/jschaf/emacs-lorem-ipsum")
