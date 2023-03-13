c8193b56d4bfb302698586e87dec64b9a6a2e0cf
Adam Porter <adam@alphapapa.net>
2023-03-12 23:54:16 -0500

    Upgrade: magit-todos

---
+++ elpa/magit-todos-20230307.549/magit-todos-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from magit-todos.el  -*- no-byte-compile: t -*-
+(define-package "magit-todos" "20230307.549" "Show source file TODOs in Magit" '((emacs "26.1") (async "1.9.2") (dash "2.13.0") (f "0.17.2") (hl-todo "1.9.0") (magit "2.13.0") (pcre2el "1.8") (s "1.12.0") (transient "0.2.0")) :commit "7724259a008144b8cfc6cacdae3e764f207a03e7" :authors '(("Adam Porter" . "adam@alphapapa.net")) :maintainer '("Adam Porter" . "adam@alphapapa.net") :keywords '("magit" "vc") :url "http://github.com/alphapapa/magit-todos")
