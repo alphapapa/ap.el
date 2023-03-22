3e6c8bc795037aa627c6de9088c9efd1654a4f14
Adam Porter <adam@alphapapa.net>
2023-03-22 11:19:01 -0500

    Upgrade: doom-themes

---
+++ elpa/doom-themes-20230221.610/doom-themes-pkg.el
@@ -0,0 +1,13 @@
+(define-package "doom-themes" "20230221.610" "an opinionated pack of modern color-themes"
+  '((emacs "25.1")
+    (cl-lib "0.5"))
+  :commit "4c239dbe42232e81c28901000f435746de2a55ab" :authors
+  '(("Henrik Lissner" . "contact@henrik.io"))
+  :maintainer
+  '("Henrik Lissner" . "contact@henrik.io")
+  :keywords
+  '("themes" "faces")
+  :url "https://github.com/doomemacs/themes")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
