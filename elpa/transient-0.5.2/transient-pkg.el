26c6b57641f4fe864ac5912537d92116af856e4e
Adam Porter <adam@alphapapa.net>
2024-01-24 02:08:20 -0600

    Upgrade: Magit, etc.

--- elpa/transient-0.5.2/transient-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from transient.el  -*- no-byte-compile: t -*-
-(define-package "transient" "0.5.2" "Transient commands" '((emacs "26.1") (compat "29.1.4.4") (seq "2.24")) :commit "2c8b50231b11d354b60709c361f2dfbc4cb8163d" :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :keywords '("extensions") :url "https://github.com/magit/transient")
