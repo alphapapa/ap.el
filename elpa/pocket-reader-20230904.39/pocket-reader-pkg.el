ef5351788f3a31e9658faf796c2594e74babf6cb
Adam Porter <adam@alphapapa.net>
2024-02-15 23:47:03 -0600

    Upgrade: pocket-reader

--- elpa/pocket-reader-20230904.39/pocket-reader-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from pocket-reader.el  -*- no-byte-compile: t -*-
-(define-package "pocket-reader" "20230904.39" "Client for Pocket reading list" '((emacs "25.1") (dash "2.13.0") (kv "0.0.19") (peg "1.0.1") (pocket-lib "0.1") (s "1.10") (ov "1.0.6") (rainbow-identifiers "0.2.2") (org-web-tools "0.1") (ht "2.2")) :authors '(("Adam Porter" . "adam@alphapapa.net")) :maintainer '("Adam Porter" . "adam@alphapapa.net") :keywords '("pocket") :url "https://github.com/alphapapa/pocket-reader.el")
---
+++ elpa/pocket-reader-20230904.539/pocket-reader-pkg.el
@@ -0,0 +1,23 @@
+(define-package "pocket-reader" "20230904.539" "Client for Pocket reading list"
+  '((emacs "25.1")
+    (dash "2.13.0")
+    (kv "0.0.19")
+    (peg "1.0.1")
+    (pocket-lib "0.1")
+    (s "1.10")
+    (ov "1.0.6")
+    (rainbow-identifiers "0.2.2")
+    (org-web-tools "0.1")
+    (ht "2.2"))
+  :commit "ef6b6892ef13eff3479d79c7f6bc918dd0444e88" :authors
+  '(("Adam Porter" . "adam@alphapapa.net"))
+  :maintainers
+  '(("Adam Porter" . "adam@alphapapa.net"))
+  :maintainer
+  '("Adam Porter" . "adam@alphapapa.net")
+  :keywords
+  '("pocket")
+  :url "https://github.com/alphapapa/pocket-reader.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
