0c6fd031a8f0f0785be30f727f48d838d56318a8
Adam Porter <adam@alphapapa.net>
2024-03-17 06:13:48 -0500

    Add: (defrepeater)

---
+++ elpa/defrepeater-20180830.410/defrepeater-pkg.el
@@ -0,0 +1,15 @@
+(define-package "defrepeater" "20180830.410" "Easily make commands repeatable"
+  '((emacs "25.2")
+    (s "1.12.0"))
+  :commit "62b00ede57d2e115b9ef9f21268c021ae1186873" :authors
+  '(("Adam Porter" . "adam@alphapapa.net"))
+  :maintainers
+  '(("Adam Porter" . "adam@alphapapa.net"))
+  :maintainer
+  '("Adam Porter" . "adam@alphapapa.net")
+  :keywords
+  '("convenience")
+  :url "http://github.com/alphapapa/defrepeater.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
