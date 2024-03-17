78f7adb5f2f887155f4585c9bc5c1c382d5d07dd
Adam Porter <adam@alphapapa.net>
2024-03-17 06:02:44 -0500

    Add: (a) Used by frecency
    Which I should change someday...

---
+++ elpa/a-20210929.1510/a-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from a.el  -*- no-byte-compile: t -*-
+(define-package "a" "20210929.1510" "Associative data structure functions" '((emacs "25")) :commit "93e5ed8c495794d1ba3c04b43041b95ce01079b1" :authors '(("Arne Brasseur" . "arne@arnebrasseur.net")) :maintainer '("Arne Brasseur" . "arne@arnebrasseur.net") :keywords '("lisp") :url "https://github.com/plexus/a.el")
