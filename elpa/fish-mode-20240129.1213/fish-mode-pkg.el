a86df8dc81dca68eb92aeb762272ac2e32ac8285
Adam Porter <adam@alphapapa.net>
2024-03-17 06:23:23 -0500

    Add: (fish-mode)

---
+++ elpa/fish-mode-20240129.1213/fish-mode-pkg.el
@@ -0,0 +1,13 @@
+(define-package "fish-mode" "20240129.1213" "Major mode for fish shell scripts"
+  '((emacs "24"))
+  :commit "2526b1803b58cf145bc70ff6ce2adb3f6c246f89" :authors
+  '(("Tony Wang" . "wwwjfy@gmail.com"))
+  :maintainers
+  '(("Tony Wang" . "wwwjfy@gmail.com"))
+  :maintainer
+  '("Tony Wang" . "wwwjfy@gmail.com")
+  :keywords
+  '("fish" "shell"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
