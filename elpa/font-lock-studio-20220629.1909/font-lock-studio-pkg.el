fd7fb2bc059b2a48e2942e7b586de0636e25609f
Adam Porter <adam@alphapapa.net>
2024-03-17 06:20:44 -0500

    Add: (font-lock-studio)
    Might as well install it since I use it occasionally, e.g. for prism.

---
+++ elpa/font-lock-studio-20220629.1909/font-lock-studio-pkg.el
@@ -0,0 +1,14 @@
+(define-package "font-lock-studio" "20220629.1909" "interactive debugger for Font Lock keywords."
+  '((emacs "24.3"))
+  :commit "78472ae1f65721b4da17756ee7e506f3d0487033" :authors
+  '(("Anders Lindgren"))
+  :maintainers
+  '(("Anders Lindgren"))
+  :maintainer
+  '("Anders Lindgren")
+  :keywords
+  '("faces" "tools")
+  :url "https://github.com/Lindydancer/font-lock-studio")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
