72075bec74e74d26e0ee8c9675ebb2efbc53402e
Adam Porter <adam@alphapapa.net>
2024-09-30 15:18:24 -0500

    Add: rainbow-identifiers
    Required by pocket-reader.

---
+++ elpa/rainbow-identifiers-20141102.1526/rainbow-identifiers-pkg.el
@@ -0,0 +1,12 @@
+(define-package "rainbow-identifiers" "20141102.1526" "Highlight identifiers according to their names"
+  '((emacs "24"))
+  :commit "19fbfded1baa98d12335f26f6d7b20e5ae44ce2e" :authors
+  '(("Fanael Linithien" . "fanael4@gmail.com"))
+  :maintainers
+  '(("Fanael Linithien" . "fanael4@gmail.com"))
+  :maintainer
+  '("Fanael Linithien" . "fanael4@gmail.com")
+  :url "https://github.com/Fanael/rainbow-identifiers")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
