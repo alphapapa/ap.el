ccdb02af7b95a36572948d5f0656f3a82b057cde
Adam Porter <adam@alphapapa.net>
2024-10-09 23:53:05 -0500

    Upgrade: xref

---
+++ elpa/xref-1.7.0/xref-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from xref.el  -*- no-byte-compile: t -*-
+(define-package "xref" "1.7.0" "Cross-referencing commands" '((emacs "26.1")) :commit "cef848fe5f355ca34abc176739d0ace835b12eed" :url "https://elpa.gnu.org/packages/xref.html")
