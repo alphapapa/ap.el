821de4d10859b7629df3281c761947ecc8b924d3
Adam Porter <adam@alphapapa.net>
2024-10-11 23:39:21 -0500

    Add: yequake
    Not sure why I didn't have this committed before.  I guess I haven't
    used it very much with this config yet.

---
+++ elpa/yequake-20200219.2323/yequake-pkg.el
@@ -0,0 +1,15 @@
+(define-package "yequake" "20200219.2323" "Drop-down frames, like Yakuake"
+  '((emacs "25.2")
+    (dash "2.14.1"))
+  :commit "d18166e597414350117d0b82a29e509fc53c636d" :authors
+  '(("Adam Porter" . "adam@alphapapa.net"))
+  :maintainers
+  '(("Adam Porter" . "adam@alphapapa.net"))
+  :maintainer
+  '("Adam Porter" . "adam@alphapapa.net")
+  :keywords
+  '("convenience" "window-system" "frames")
+  :url "http://github.com/alphapapa/yequake")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
