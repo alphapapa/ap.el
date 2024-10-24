f4245871572d966fa8af6299ed62f9143cd51d91
Adam Porter <adam@alphapapa.net>
2024-10-23 19:41:51 -0500

    Replace: transient
    Accidentally removed this from an earlier commit.

---
+++ elpa/transient-20240415.1548/transient-pkg.el
@@ -0,0 +1,16 @@
+(define-package "transient" "20240415.1548" "Transient commands"
+  '((emacs "26.1")
+    (compat "29.1.4.4")
+    (seq "2.24"))
+  :commit "53198a3df5c29b7906576bdfa3f4fa13b9bb47e1" :authors
+  '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
+  :maintainers
+  '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
+  :maintainer
+  '("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")
+  :keywords
+  '("extensions")
+  :url "https://github.com/magit/transient")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
